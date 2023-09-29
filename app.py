a87705cb622f13e450f3e0670c9a96299921c11c
Nasr-Edine DRAI <d.nasredine@gmail.com>
2023-09-29 16:03:11 +0200

    Add a front end page for requesting API

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 import joblib
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 from text_processing_utils import preprocess_text
 
 # Define the Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Define the route for the predictor
 @app.route('/predict', methods=['POST'])
@@ -46,4 +49,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host='0.0.0.0', port=8001)
